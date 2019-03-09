726d5d421ef29a661262ee48b26d96fa9131bca0
dddeska <domicheche@gmail.com>
2019-03-09 14:12:49 +0100

    losowanie

---
+++ python/04dzien/05dzien/losowanieliczb.py
@@ -0,0 +1,20 @@
+import random
+
+
+losowa_liczba = random.randint(0, 100)
+
+zgadniete = False
+ilosc_prob = 0
+
+#komunikat o grze
+
+
+while (not zgadniete) and (ilosc_prob < 7):
+    uzyt_zgaduje = input('podaj liczbe: ')
+    uzyt_zgaduje = int(uzyt_zgaduje)
+    #if 
+    #komunikat
+    #zmienic zgadniete
+    #zmienic ilosc_prob
+
+#komunikat statystyki 
